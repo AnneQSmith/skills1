717e849ca730a3c354c92a21b27446c3be7257d9
Hackbright Student <info@hackbrightacademy.com>
2014-02-07 18:11:47 -0800

    More tests

--- test_skills.py
+++ test_skills.py
@@ -1,10 +1,5 @@
 import skills1
 
-def test_smallest():
-    a = range(5)
-    out = skills1.smallest(a)
-    print out
-    assert out == 0
 
 def test_all_odd():
     a = ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer']
@@ -18,7 +13,26 @@ def test_all_even():
     a = range(5)
     out = skills1.all_even(a)
     assert out == [0,2,4]
-    
+
+
+def test_long_words():
+    c = ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer',"wer","were"]
+    out = skills1.long_words(c)
+    assert out == ["qowieur","qiwer","were"]
+
+def test_smallest():
+
+    c = ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer',"wer","were",-999]
+    cc = [ "1","WERE",.93939]
+    out = skills1.smallest(c)
+    assert out == -999
+    out = skills1.smallest(cc)
+    assert out == None
+    a = range(5)
+    out = skills1.smallest(a)
+    assert out == 0
+
+
 
 
     
