a539b10bb409cdeb22f59084d708698605d3c445
Hackbright Student <info@hackbrightacademy.com>
2014-02-10 09:57:32 -0800

    files from home; all should work

--- skills1.py
+++ skills1.py
@@ -48,12 +48,6 @@ def long_words(word_list):
 
     return new_list
 
-
-c = ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer',"wer","were"]
-print c
-c = long_words(c)
-print c
-
 # Write a function that finds the smallest element in a list of integers and returns it.
 def smallest(some_list):
        
@@ -68,38 +62,99 @@ def smallest(some_list):
 
     return smallnumber
 
-c = ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer',"wer","were",-999,-999999999]
-cc = [ "1","WERE",.93939]
-print "before and after smallest"
-print c
-print smallest(c)
-print cc
-print smallest(cc)
 
 # Write a function that finds the largest element in a list of integers and returns it.
 def largest(some_list):
-    return None
+          
+    firstvalidnumber = None
+    largenumber = None
+    for i in range(len(some_list)):
+        if type(some_list[i]) == int:
+            if firstvalidnumber == None:
+                largenumber = firstvalidnumber = some_list[i]
+            if some_list[i] > largenumber:
+                largenumber = some_list[i]
+
+    return largenumber
 
 # Write a function that takes a list of numbers and returns a new list of all those numbers divided by two.
 def halvesies(some_list):
-    return []
+
+    new_list = []
+    for i in range(len(some_list)):
+        
+        if type(some_list[i]) == (int or float):
+             new_list.append(some_list[i]/2.0)
+
+    return new_list
+
 
 # Write a function that takes a list of words and returns a list of all the lengths of those words.
 def word_lengths(word_list):
-    return []
+    new_list = []
+    for i in range(len(word_list)):
+        if type(word_list[i]) == str:
+            new_list.append(len(word_list[i]))
+
+    return new_list
 
 # Write a function (using iteration) that sums all the numbers in a list.
 def sum_numbers(numbers):
-    return []
+
+    summation = 0
+
+    print len(numbers)
+    for i in range(len(numbers)):
+        
+        if type(numbers[i]) == int:
+            summation += numbers[i]
+        elif type(numbers[i]) == float:
+            summation += numbers[i]
+    return summation
+
 
 # Write a function that multiplies all the numbers in a list together.
 def mult_numbers(numbers):
-    return []
+    prodnumbers = 1
+    for i in range(len(numbers)):
+    
+        if type(numbers[i]) == float:
+            prodnumbers *= numbers[i] 
+        elif type(numbers[i]) == int:
+            prodnumbers *= numbers[i]
+
+    return prodnumbers
+
 
 # Write a function that joins all the strings in a list together (without using the join method) and returns a single string.
 def join_strings(string_list):
-    return ""
+
+    newstring = ''
+    for i in range(len(string_list)):
+        if type(string_list[i]) == str:
+
+            newstring += string_list[i]
+
+    return newstring
+
 
 # Write a function that takes a list of integers and returns the average (without using the avg method)
 def average(numbers):
-    return None
+
+    summation = 0
+    j = 0
+    aver = 0.0
+
+    for i in range(len(numbers)):
+        if type(numbers[i]) == int:
+            summation += numbers[i]
+            j +=1
+
+    if j > 0:
+        aver = float(summation) / float(j)
+    else:
+        aver = None
+
+
+    return aver
+
--- test_skills.py
+++ test_skills.py
@@ -33,9 +33,85 @@ def test_smallest():
     assert out == 0
 
 def test_largest():
-    a = range(5)
-    out = skills1.largest(a)
-    assert out == 4
+    c =  ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer',"wer","were",-999]
+    cc = [ "1","WERE",.99]
+    d = range(9)
+    out = skills1.largest(c)
+    assert out == 9
+    out = skills1.largest(cc)
+    assert out == None
+    out = skills1.largest(d)
+    assert out == 8
+
+def test_halvesies():
+    c =  ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer',"wer","were",-999]
+    cc = [ "1","WERE",.99]
+    d = range(9)
+    out = skills1.halvesies(d)
+    assert out == [0/2.0,1/2.0,2/2.0,3/2.0,4/2.0,5/2.0,6/2.0,7/2.0,8/2.0]
+    out = skills1.halvesies(c)
+    assert out == [4/2.0,5/2.0,6/2.0,7/2.0,8/2.0,9/2.0,-999/2.0]
+
+
+def test_word_lengths():
+    c =  ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer',"wer","were",-999]
+    cc = [ "1","WERE",.99]
+    out = skills1.word_lengths(c)
+    assert out == [1,1,1,1,7,5,3,4]
+    out = skills1.word_lengths(cc)
+    assert out == [1,4]
+    d = range(9)
+    out = skills1.word_lengths(d)
+    assert out == []
+
+def test_sum_numbers():
+    c =  ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer',"wer","were",-999]
+    cc = [ "1","WERE", 0.99]
+    out = skills1.sum_numbers(c)
+    assert out == 4+5+6+7+8+9-999
+    out = skills1.sum_numbers(cc)
+    assert out == 0.99
+
+def test_mult_numbers():
+    c =  ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer',"wer","were",-999]
+    cc = [ "1","WERE", 0.99]
+    out = skills1.mult_numbers(c)
+    assert out ==  4*5*6*7*8*9*-999
+    out = skills1.mult_numbers(cc)
+    assert out == 0.99
+
+def test_join_strings():  
+    c = ['1','2','2','3',4,5,6,7,8,9,'qowieur','qiwer',"wer","were",-999]
+    cc = [ "1","WERE", 0.99]
+    out = skills1.join_strings(c)
+    assert out == '1223qowieurqiwerwerwere'
+    out = skills1.join_strings(cc)
+    assert out == '1WERE'
+
+
+
+
+def averages():
+
+    c = range(9)
+    out = skills1.average(c)
+    assert out == 4.5
+    cc = [1.0,2.0]
+    out = skills.average(cc)
+    assert out == 1.5
+    ccc = ['were','owieure','woeirue']
+    out = skills1.average(ccc)
+    assert out == None
+
+
+
+
+
+
+
+
+
+    
 
 
 
